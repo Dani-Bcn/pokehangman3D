6e0f2e2854aed16e49321b0916376352c92f2392
Dani-Bcn <nneodani@gmail.com>
2023-11-18 14:51:36 +0100

    test

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function About() {
+  const navigate = useNavigate()
   return (
-    <div>About</div>
+    <main>
+      <h2>About</h2>
+    <button onClick={()=> navigate("/")} className='p-5'>Back</button>
+    </main>
   )
 }
